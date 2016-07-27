9e245656e3181cae44a99624502dab892ad72675
Lane Anderson <lane.anderson@moody.edu>
2016-07-27 09:50:08 -0500

    updated scrolling nav and about page dynamic revealing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
 //NAVBAR SCROLLING CHANGE
-$('#givingIcons').hover( function(){
-	$('.badge.giving span').hide();
-	$('.badge.giving .fa.fa-money').fadeIn("slow");
-}, function(){
-	$('.badge.giving .fa.fa-money').hide();
-	$('.badge.giving span').fadeIn("slow");
+if ( $(window).width() >= 768) {
+	var scroll_start = 0;
+	   var startchange = $('.navbar-inverse');
+	   var offset = startchange.offset();
+	   $(document).scroll(function() {
+	      scroll_start = $(this).scrollTop();
+	      if(scroll_start > offset.top + 50) {
+
+					$('#dbLogo').hide( function() {
+						$('.navbar-inverse').animate({
+							marginTop: '-35px'
+						}, 250, function() {
+							$('.navbar-inverse').css('background-color', '#ff9900').finish()
+						} );
+					});
+
+				} else {
+
+					$('.navbar-inverse').css('background-color', 'transparent').finish()
+	 				$('.navbar-inverse').animate({
+						marginTop: '30px'
+					}, 500);
+					$('#dbLogo').fadeIn();
+	       }
+
+	   });
+
+}
+else {
+  //Add your javascript for small screens here
+};
+
+$('div.belief h3').click( function() {
+	$(this).siblings().toggleClass('hidden');
 });
